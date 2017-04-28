a1b90ae4ed6d33859c37716427246d370b7ae799
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-28 16:12:25 -0400

    add all listeners for events from client

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 var fs = require('fs')
-var db = JSON.parse(fs.readFileSync('db/content.json', 'utf8'))
+var content = JSON.parse(fs.readFileSync('db/content.json', 'utf8'))
+var db = require('diskdb')
+db.connect('db', ['names', 'option1', 'option2', 'option3'])
 
 app.use(express.static('public'))
 
@@ -20,23 +22,52 @@ io.on('connection', (socket) => {
     console.log('Socket w/ client successful')
 
     socket.on('welcomeMessage', () => {
-        socket.emit('botMessage', {data: db.welcomeMessage.message})
+        socket.emit('botMessage', {data: content.welcomeMessage.message})
         var menuButtons = function() {
-            return socket.emit('menuButtons', {data: db.welcomeMessage.options})
+            return socket.emit('menuButtons', {data: content.welcomeMessage.options})
         }
         setTimeout(menuButtons, 1200)
     })
 
     socket.on('menuRequest', (data) => {
-      console.log(data.data);
-        socket.emit('botMessage', {
-            data: db[data.data].message
-        })
-        socket.emit('menuButtons', {
-            data: db[data.data].options
-        })
+        switch (data.data) {
+            case "Yes":
+              socket.emit('botMessage', {
+                data: content[data.data].message
+              })
+              break;
+            default:
+              if (content[data.data].gif) {
+                console.log('you clicked a person!')
+                socket.emit('gif', {data: content[data.data].gif})
+              }
+              socket.emit('botMessage', {
+                data: content[data.data].message
+              })
+              socket.emit('menuButtons', {
+                data: content[data.data].options
+              })
+        }
+    })
+    socket.on('saveNames', (data) => {
+        var respondant = {
+            name: data.name,
+            saved: new Date()
+        }
+        db.names.save(respondant)
+        console.log('Person saved!')
+        socket.emit('botMessage', {data: "Thank You, please choose an option"})
+        socket.emit('menuButtons', {data: ["New Hires", "Creative 101"]})
     })
 
+    socket.on('vote', (data) => {
+      var vote = {
+        time: new Date()
+      }
+      db[data.data].save(vote)
+      socket.emit('botMessage', {data: "Thank You, please choose an option"})
+      socket.emit('menuButtons', {data: ["New Hires", "Creative 101"]})
+    })
 })
 
 var port = process.env.PORT || 3000
