d47f9b6fdf8727b6c950cf6f00ea35ce03a905af
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-28 10:17:54 -0400

    add logic for client to make request to server for content

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,27 @@
 $(document).ready(() => {
   var socket = io.connect();
+
+   $('.main-content').fadeIn(200)
+   .promise()
+   .done(() => {
+     socket.emit('welcomeMessage')
+   })
+
+   socket.on('botMessage', (data) => {
+        $('.messages').prepend("<p class='bot-message'>" + data.data + "</p>").hide().fadeIn('slow');
+    })
+
+    socket.on('menuButtons', (data) => {
+        for (var i = 0; i < data.data.length; i++) {
+            $('.btns').append("<button type='button' value='" + data.data[i] + "'>" + data.data[i] + "</button>")
+        }
+    })
+
+    $('.btns').click(() => {
+        socket.emit('menuRequest', {data: event.target.value})
+        $('.btns').empty()
+        $('.messages').empty()
+    })
+
+   // end of DOM ready
 })
