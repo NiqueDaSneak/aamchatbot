c1da60c1bcfa6d69e7a3be88078727a882663b27
Dom Clemmer <thisisdom4@gmail.com>
2017-05-03 15:17:33 -0400

    hide everything on a desktop

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,31 @@
 $(document).ready(() => {
+
+  var isMobile = {
+      Android: function() {
+          return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function() {
+          return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function() {
+          return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function() {
+          return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
+          return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function() {
+          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+  }
+
+  if (!isMobile.any()) {
+    $('body').empty().append("<div class='cover'><p>Please view this on a mobile device only</p></div>")
+
+  }
+
     var socket = io.connect();
 
     socket.emit('welcomeMessage')
