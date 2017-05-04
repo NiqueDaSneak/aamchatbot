942539721f83ff062c9b820d8cdeca8fc0563056
Dom Clemmer <thisisdom4@gmail.com>
2017-05-04 14:34:43 -0400

    add fix so rotation can happen both ways

--- public/js/index.js
+++ public/js/index.js
@@ -31,6 +31,10 @@ $(document).ready(() => {
       $('.hide-orientation').fadeIn()
     }
 
+    if (window.orientation === -90) {
+      $('.hide-orientation').fadeIn()
+    }
+
     if (window.orientation === 0) {
         $('.hide-orientation').fadeOut()
     }
